c837627b9b2c1d875c628f94b8ea490d4c243fe5
binayakpanda2003 <me.binayakpanda@gmail.com>
2025-09-03 10:30:25 +0530

    Time: 5 ms (65.52%) | Memory: 44.1 MB (60.42%) - LeetSync

---
+++ 9-palindrome-number/palindrome-number.java
@@ -0,0 +1,24 @@
+class Solution {
+    public boolean isPalindrome(int x) {
+       int revNum = 0;
+       
+        int dup = x;
+     
+        while (x > 0) {
+          
+            int ld = x % 10;
+           
+            revNum = (revNum * 10) + ld;
+         
+            x = x / 10;
+        }
+      
+        if (dup == revNum) {
+           
+            return true;
+        } else {
+         
+            return false;
+        }
+    }
+}
